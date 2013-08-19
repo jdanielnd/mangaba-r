649cb5440dbae1b8df7882aec9b6f948ab31255e
João Daniel <jdanielnd@gmail.com>
2013-08-19 12:28:55 -0300

    changing weighting to MAE and returing errors measures

--- R/ens.forecast.R
+++ R/ens.forecast.R
@@ -1,23 +1,24 @@
 ens.forecast <-
 function(x, start, frequency, level) {
 
+  # Creating Time Series object
   tser = ts(x, start=start, frequency=frequency)
+  # Getting series size
   size = length(tser)
 
+
+  # Seasonal is disabled if the size is smaller then 2*frequency
   if (length(x) < frequency*2) {
     mod.aa = auto.arima(tser, seasonal=FALSE)
     mod.aach = mod.aa
     mod.ee = ets(tser, model="ZZN")
   } else {
     mod.aa = auto.arima(tser)
-    if (length(x) > (frequency+1)) {
-      mod.aach = auto.arima(tser, seasonal.test="ch")
-    } else {
-      mod.aach = mod.aa
-    }
+    mod.aach = auto.arima(tser, seasonal.test="ch")
     mod.ee = ets(tser)
   }
 
+  # Set the default horizont for each type of frequency
   if(frequency == 12) {
     h <- 12
   } else if (frequency == 4) {
@@ -30,29 +31,58 @@ function(x, start, frequency, level) {
     h <- 12
   }
 
+  # Set the horizont to the minimun between the series size and the default horizont
   h <- min(size,h)
 
+  # Calculate forecasts
   fc.aa = forecast(mod.aa, level=level, h=h)
   fc.aach = forecast(mod.aach, level=level, h=h)
   fc.ee = forecast(mod.ee, level=level, h=h)
-  mape.aa = accuracy(mod.aa)[["MAPE"]]
-  mape.aach = accuracy(mod.aach)[["MAPE"]]
-  mape.ee = accuracy(mod.ee)[["MAPE"]]
-  mape.aa = ifelse(is.infinite(mape.aa), 1, mape.aa)
-  mape.aach = ifelse(is.infinite(mape.aach), 1, mape.aach)
-  mape.maa = (mape.aa + mape.aach)/2
-  mape.ee = ifelse(is.infinite(mape.ee), 1, mape.ee)
-  sum.mape = mape.maa + mape.ee
-  if(sum.mape == 0 || is.nan(sum.mape)) {
-    upper = fc.ee$upper
-    point = fc.ee$mean
-    lower = fc.ee$lower
-  } else {
+
+  # Calculate MAPE's
+  mae.aa = accuracy(mod.aa)[["MAE"]]
+  mae.aach = accuracy(mod.aach)[["MAE"]]
+  mae.ee = accuracy(mod.ee)[["MAE"]]
+
+  # Correct MAPE's - If it's infinite, replace by 1
+  # We don't need to correct the MAPE anymore, because MAE doesn't have
+  # the 0/infinity problem
+  # mae.aa = ifelse(is.infinite(mape.aa), 1, mape.aa)
+  # mae.aach = ifelse(is.infinite(mape.aach), 1, mape.aach)
+  # mae.ee = ifelse(is.infinite(mape.ee), 1, mape.ee)
+  
+  # Calculate mean MAE of ARIMAS
+  mae.maa = (mae.aa + mae.aach)/2
+
+  # Calculate sum of MAE's
+  sum.mae = mae.maa + mae.ee
+  
+  # If MAPE's are zero or NaN use ETS
+  # We don't need this correction anymore, because
+  # the MAE always exists.
+  # if(sum.mae == 0 || is.nan(sum.me)) {
+  #   upper = fc.ee$upper
+  #   point = fc.ee$mean
+  #   lower = fc.ee$lower
+  # } else {
+  # Else combine models  
     weight.maa = mape.ee/sum.mape
     weight.ee = mape.maa/sum.mape
     upper = (fc.aa$upper*0.5 + fc.aach$upper*0.5)*weight.maa + fc.ee$upper*weight.ee
     point = (fc.aa$mean*0.5 + fc.aach$mean*0.5)*weight.maa + fc.ee$mean*weight.ee
     lower = (fc.aa$lower*0.5 + fc.aach$lower*0.5)*weight.maa + fc.ee$lower*weight.ee
-  }
-  list(sup=upper,point,inf=lower)
+  # }
+
+  # Calculate MAPE of ensembled model
+  
+  # Get fitted of each model
+  fitted.aa <- fitted(mod.aa)
+  fitted.aach <- fitted(mod.aach)
+  fitted.ee <- fitted(mod.ee)
+
+  # Calculate fitted of ensebled model
+  fitted.ens <- (fitted.aa*0.5 + fitted.aach*0.5)*weight.maa + fitted.ee*weight.ee
+  errors <- accuracy(f=fitted.ens, x=tser)[,c("MAE","MAPE")]
+
+  list(sup=upper,point,inf=lower,errors=errors)
 }
